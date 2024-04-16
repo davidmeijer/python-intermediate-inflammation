0f79efbceebd5282aa8976d9de830cbadc4dc422
David Meijer <david-meijer@live.nl>
2024-04-16 12:02:49 +0200

    Add function for calculating if daily inflammation value exceeds given treshold

--- inflammation/models.py
+++ inflammation/models.py
@@ -79,3 +79,13 @@ def std_dev(data):
     :returns: An array of standard deviation values of measurements for each day.
     """
     return np.std(data, axis=0)
+
+def daily_above_threshold(patient_num, data, threshold):
+    """Determine whether or not each daily inflammation value exceeds a given threshold for a given patient.
+
+    :param patient_num: The patient row number
+    :param data: A 2D data array with inflammation data
+    :param threshold: An inflammation threshold to check each daily value against
+    :returns: A boolean list representing whether or not each patient's daily inflammation exceeded the threshold
+    """
+    return list(map(lambda x: x > threshold, data[patient_num]))
